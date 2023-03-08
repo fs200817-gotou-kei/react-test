07d6d8935c4d3a83f143fbfe85d6c2f57666413c
LAPTOP-N8E0I7QK\gotou <gotoukei57@gmail.com>
2023-03-09 05:48:35 +0900

    一旦クリックされたときにeventsに値が入る動作は確認できた。 ただeventsが表示されない

--- test/src/ui/components/TemperatureCrudPage.jsx
+++ test/src/ui/components/TemperatureCrudPage.jsx
@@ -4,18 +4,30 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
 export default class TemperatureCrudPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            events: {
+                title: 'fource',
+                start: '2023-03-01',
+                end: '2023-03-02',
+            }
+        }
+    }
 
     render() {
         return (
             <FullCalendar
                 plugins={[dayGridPlugin, interactionPlugin]}
                 dateClick={this.handleDateClick}
+                events={this.state.events}
             />
         )
     }
 
-    handleDateClick = (arg) => { // bind with an arrow function
+    handleDateClick = (arg) => {
+        console.log(this.state.events)
+        this.state.events.end = '2023-03-20'
         alert(arg.dateStr)
     }
-
 }
